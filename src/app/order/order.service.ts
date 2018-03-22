90cb50c1a3ebe2a76ec1020ea0023cce5cb96aaf
Jonatan Gall <jonatangd.souza@gmail.com>
2018-03-22 20:28:51 -0300

    seção 6 aula 70

--- src/app/order/order.service.ts
+++ src/app/order/order.service.ts
@@ -1,29 +1,51 @@
 import { Injectable } from "@angular/core";
+import { Http, Headers, RequestOptions } from "@angular/http";
+import { Observable } from "rxjs/Observable";
+import "rxjs/add/operator/map";
+
 import { ShoppingCartService } from "../restaurant-detail/shopping-cart/shopping-cart.service";
 import { CartItem } from "../restaurant-detail/shopping-cart/cart-item.model";
+import { Order, OrderItem } from "./order.model";
+
+import { MEAT_API } from "../app.api";
 
 @Injectable()
 export class OrderService {
-  constructor (private cartService: ShoppingCartService){
+  constructor(private cartService: ShoppingCartService, private http: Http) {}
 
-  }
-
-  cartItems(): CartItem[]{
+  cartItems(): CartItem[] {
     return this.cartService.items;
   }
 
-  increaseQty(item: CartItem){
-    this.cartService.increaseQty(item)
+  increaseQty(item: CartItem) {
+    this.cartService.increaseQty(item);
   }
-  decreaseQty(item: CartItem){
-    this.cartService.decreaseQty(item)
-  }
-
-  remove(item: CartItem){
-    this.cartService.removeItem(item)
+  decreaseQty(item: CartItem) {
+    this.cartService.decreaseQty(item);
   }
 
-  itemsValue(): number{
-    return this.cartService.total()
+  remove(item: CartItem) {
+    this.cartService.removeItem(item);
+  }
+
+  itemsValue(): number {
+    return this.cartService.total();
+  }
+
+  checkOrder(order: Order): Observable<string> {
+    const headers = new Headers();
+    headers.append("Content-Type", "application/json");
+
+    return this.http
+      .post(
+        `${MEAT_API}/orders`,
+        JSON.stringify(order),
+        new RequestOptions({ headers: headers })
+      )
+      .map(response => response.json())
+      .map(order=> order.id)
+  }
+  clear() {
+    this.cartService.clear();
   }
 }

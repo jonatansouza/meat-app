5a526e572b5de798c3dd307a3a9052d7d61e39d2
Jonatan Gall <jonatangd.souza@gmail.com>
2018-04-09 21:04:21 -0300

    secao 15 aula 117

--- backend/auth.ts
+++ backend/auth.ts
@@ -2,19 +2,24 @@ import {Request, Response} from 'express'
 
 import {User, users} from './users'
 
+import * as jwt from 'jsonwebtoken'
+
+import {apiConfig} from './api-config'
+
 export const handleAuthentication = (req: Request, res: Response) =>{
   const user = req.body;
 
-  if(isValid(user)){
-    const dbUser: User = users[user.email]
-    res.json(dbUser)
-  }else{
+  if ( isValid(user)) {
+    const dbUser = users[user.email]
+    const token = jwt.sign({sub: dbUser.email, iss: 'meat-api'}, apiConfig.secret)
+    res.json({name: dbUser.name, email: dbUser.email, accessToken: token})
+  } else {
     res.status(403).json({message: 'Dados inválidos.'})
   }
 }
 
 function isValid(user): boolean{
-  if(!user){
+  if ( !user) {
     return false;
   }
   const dbUser = users[user.email]
